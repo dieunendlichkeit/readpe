ed4307356343401cf672eb603de80abb466418db
Luiz M <81941217+dieunendlichkeit@users.noreply.github.com>
2023-07-12 22:36:40 -0300

    Update petest.c

--- lib/petest.c
+++ lib/petest.c
@@ -1,14 +1,13 @@
-	#include "petest.h"
+#include "petest.h"
 
 bool petest_ispe(PEFILE *pe){
     return (pe->hdr_dos->e_magic == 0x5a4d);
 }
 
 bool petest_init(PEFILE *pe) {
-         FILE *f = fopen(pe->filepath, "rb");
-
+	FILE *f = fopen(pe->filepath, "rb");
         if(!f)
-                return false;
+		return false;
 
         pe->hdr_dos = malloc(sizeof(IMAGE_DOS_HEADER));
         if (pe->hdr_dos == NULL)
